194d079e9d313798a37d38024bcc0d01f1b61d2d
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-09-23 13:43:21 +0000

    properly set font on pixel, dont force bf to freestyle

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -201,6 +201,7 @@ class PlayState extends MusicBeatState
 	public static var lastCombo:FlxSprite;
 	// stores the last combo score objects in an array
 	public static var lastScore:Array<FlxSprite> = [];
+	var curFont = null; //to properly set the font on format
 	override public function create()
 	{
 		PauseSubState.songName = null; //Reset to default
@@ -328,6 +329,9 @@ class PlayState extends MusicBeatState
 		dadGroup = new FlxTypedGroup<Character>();
 		gfGroup = new FlxTypedGroup<Character>();
 
+		if(curFont == null)
+			curFont = (isPixelStage ? Paths.font("pixel.otf") : Paths.font("vcr.ttf"));
+		
 		switch (curStage)
 		{
 			case 'stage': //Week 1
@@ -709,7 +713,7 @@ class PlayState extends MusicBeatState
 		strumLine.scrollFactor.set();
 
 		timeTxt = new FlxText(STRUM_X + (FlxG.width / 2) - 248, 20, 400, "", 32);
-		timeTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		timeTxt.setFormat(curFont, 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		timeTxt.scrollFactor.set();
 		timeTxt.alpha = 0;
 		timeTxt.borderSize = 2;
@@ -804,14 +808,14 @@ class PlayState extends MusicBeatState
 		reloadHealthBarColors();
 
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
-		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		scoreTxt.setFormat(curFont, 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
 		scoreTxt.borderSize = 1.25;
 		scoreTxt.visible = !ClientPrefs.hideHud;
 		add(scoreTxt);
 
 		botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "BOTPLAY", 32);
-		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		botplayTxt.setFormat(curFont, 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		botplayTxt.scrollFactor.set();
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
@@ -2844,7 +2848,7 @@ class PlayState extends MusicBeatState
 				var index = pressArray.indexOf(pressArray.contains(true));
 				if(pressArray[index])
 				{
-					boyfriend.playAnim(singAnims[index], true);
+					boyfriend.playAnim(singAnims[index]);
 				}
 			}
 		}
@@ -2958,7 +2962,7 @@ class PlayState extends MusicBeatState
 				var index = controlArray.indexOf(controlArray.contains(true));
 				if(controlArray[index])
 				{
-					boyfriend.playAnim(singAnims[index], true);
+					boyfriend.playAnim(singAnims[index]);
 				}
 			}
 		}
