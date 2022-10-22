73c09eab376c4a0b1071944fd9b678ebe7d8a008
SanicBTW <sansunder718jqda@gmail.com>
2022-10-22 23:14:33 +0200

    1.2 quick fix

--- source/features/OnlineSongSelection.hx
+++ source/features/OnlineSongSelection.hx
@@ -83,7 +83,7 @@ class OnlineSongSelection extends MusicBeatState
         #end
 
         #if android
-        addVirtualPad(LEFT_FULL, A_B);
+        addVirtualPad(UP_DOWN, A_B);
         #end
     }
 
